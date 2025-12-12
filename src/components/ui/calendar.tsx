c5f0c2b29f943b7d0838337bc592686334b0d06a
nicontrerasc8@gmail.com <nicontrerasc8@gmail.com>
2025-12-12 10:29:28 -0500

    DayPicker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -6,12 +6,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}: React.ComponentProps<typeof DayPicker>) {
+function Calendar() {
   return ""
 }
 
