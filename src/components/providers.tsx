8bd23062998f449d613c9800af40afc79f8b0fbb
nicontrerasc8@gmail.com <nicontrerasc8@gmail.com>
2025-12-12 10:19:51 -0500

    .....

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+
 import { ApolloProvider } from '@apollo/client';
 import { client } from '@/lib/apollo-client';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+
       <ApolloProvider client={client}>
         {children}
       </ApolloProvider>
-    </SessionProvider>
+
   );
 } 
